4aae58b8f3147f43d446a19446334ad1180836b8
ruran8wa <p.rurangwa@alustudent.com>
2026-03-02 13:01:50 +0200

    chore: remove unused legacy screen files

--- frontend/src/screens/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BuildingsScreen } from "./BuildingsScreen";
-export { default as ExploreScreen } from "./ExploreScreen";
-export { default as ProfileScreen } from "./ProfileScreen";
-
